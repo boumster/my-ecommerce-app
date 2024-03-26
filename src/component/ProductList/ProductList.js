d954c8c036fd876ab2bf6d8f93d82b94df8e8d1b
vgouttin7 <victor.gouttin@ucalgary.ca>
2024-03-25 22:49:55 -0600

    fixed

--- src/component/ProductList/ProductList.js
+++ src/component/ProductList/ProductList.js
@@ -1,12 +1,12 @@
 // ProductList.js
 import React from 'react';
-import ProductItem from './ProductItem/ProductItem';
-import productsData from '../data/product';
+import ProductItem from '../ProductItem/ProductItem.js';
+import product from '../../data/product.js';
 
 const ProductList = ({ addToCart }) => {
   return (
     <div className="product-list">
-      {productsData.map((product, index) => (
+      {product.map((product, index) => (
         <ProductItem key={index} product={product} addToCart={addToCart} />
       ))}
     </div>
