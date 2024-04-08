d70782cdfa9a57ae2d27402cf20ff9841788fa29
vgouttin7 <victor.gouttin@ucalgary.ca>
2024-04-08 12:51:37 -0600

    backend

--- Frontend/src/component/ProductList/ProductList.js
+++ Frontend/src/component/ProductList/ProductList.js
@@ -1,16 +1,38 @@
 // ProductList.js
-import React from 'react';
-import ProductItem from '../ProductItem/ProductItem.js';
-import product from '../../data/product.js';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
-const ProductList = ({ addToCart }) => {
-  return (
-    <div className="product-list">
-      {product.map((product, index) => (
-        <ProductItem key={index} product={product} addToCart={addToCart} />
-      ))}
-    </div>
-  );
+const ProductList = () => {
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        axios.get('http://localhost:5000/products')
+            .then(response => {
+                console.log('Response:', response);
+                setProducts(response.data);
+            })
+            .catch(error => {
+                console.error('Error fetching products:', error);
+            });
+    }, []);
+
+    console.log('Products:', products);
+
+    return (
+        <div>
+            <h1>Product List</h1>
+            <ul>
+                {products.map(product => (
+                    <li key={product.id}>
+                        <img src={product.image} alt={product.name} />
+                        <p>{product.name}</p>
+                        <p>{product.description}</p>
+                        <p>${product.price}</p>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
 };
 
 export default ProductList;
